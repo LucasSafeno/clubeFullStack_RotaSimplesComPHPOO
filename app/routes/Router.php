06a608356a4468d8581d90b03c0576802bb6719c
Lucas Safeno <lugnorio@gmail.com>
2025-05-12 13:27:50 -0300

    Rotas Simples com PHP Orientado a Objetos#03 - Criando as rotas

--- app/routes/Router.php
+++ app/routes/Router.php
@@ -4,5 +4,23 @@ namespace app\routes;
 
 class Router
 {
-    
+
+    public static function load(string $controller, string $method)
+    {
+
+    }
+
+    public static function routes(array $routes)
+    {
+        return [
+            'get' => [
+                '/' => self::load('HomeController', 'index'),
+                '/contact' => self::load('ContactController', 'index')
+
+            ],
+            'post' => [
+                '/contact' => self::load('ContactController', 'store')
+            ],
+        ];
+    }
 }
